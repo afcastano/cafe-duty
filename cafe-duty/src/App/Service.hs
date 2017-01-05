f0fd4a277066b4085baa2d6a53a490640a20be82
afcastano <andresfelipecastano@gmail.com>
2017-01-06 02:18:49 +1100

    refactor code

--- cafe-duty/src/App/Service.hs
+++ cafe-duty/src/App/Service.hs
@@ -3,33 +3,29 @@ module App.Service (fetchTeam, currentDuty, nextDuty, allDuties) where
 import App.RosterGeneration
 import App.Repository
 import App.Helper.Lists (rotate)
-import Data.Maybe
 
 fetchTeam :: String -> IO (Maybe Team) -- Return a result object better
 fetchTeam name = findTeam name
 
 currentDuty :: String -> IO [Person]
 currentDuty teamName = do
-          maybeTeam <- findTeam teamName
-          return $ ((!!0).generateRosterFrom) `bindToMaybe` maybeTeam -- make !!0 safer
+          roster <- allDuties teamName
+          return $ roster !! 0 -- make !! 0 safer
 
 nextDuty :: String -> IO [Person]
 nextDuty teamName = do
-          maybeTeam <- findTeam teamName
-          return $ ((!!1).generateRosterFrom) `bindToMaybe` maybeTeam -- make !!1 safer
+          roster <- allDuties teamName
+          return $ roster !! 1 -- make !! 1 safer
           
 allDuties :: String -> IO [[Person]]
 allDuties teamName = do
           maybeTeam <- findTeam teamName
-          return $ generateRosterFrom `bindToMaybe` maybeTeam
+          return $ generateRosterFrom maybeTeam
 
 
 ---------- Helper functions----
-generateRosterFrom :: Team -> [[Person]]
-generateRosterFrom team = 
-                  let roster  = generateRoster $ members team
-                  in rotate (rosterIndex team) roster
-
-bindToMaybe :: (a -> [b]) -> Maybe a -> [b]
-bindToMaybe f Nothing  = []
-bindToMaybe f (Just a) = f a 
+generateRosterFrom :: Maybe Team -> [[Person]]
+generateRosterFrom Nothing = [] 
+generateRosterFrom (Just team) = 
+                                let roster  = generateRoster $ members team
+                                in rotate (rosterIndex team) roster
