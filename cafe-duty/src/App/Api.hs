d2d115b0d4eed0993821e033bc8ba99c14718c99
afcastano <andresfelipecastano@gmail.com>
2017-01-09 01:07:18 +1100

    Added basic tempalting and index.html

--- cafe-duty/src/App/Api.hs
+++ cafe-duty/src/App/Api.hs
@@ -2,10 +2,15 @@
 module App.Api (webApi) where
 
 import App.Service
+import App.Repository(Team(..), Person(..))
 
 import Web.Scotty
 import Control.Monad.IO.Class
 import Data.Aeson (ToJSON)
+import Text.Hastache
+import Text.Hastache.Context
+import qualified Data.Text.Lazy.IO as TL
+import Data.Text.Lazy
 
 webApi :: ScottyM()
 webApi = do
@@ -32,8 +37,22 @@ webApi = do
     name <- param "name"
     returnJson $ getAllDuties name
 
+  get "/web/team/:name" $ do
+    teamName <- param "name"
+    currentDuty <- liftAndCatchIO $ currentDuty teamName
+    let context "name"    = MuVariable teamName
+        context "person1" = MuVariable $ name (currentDuty !! 0)
+        context "person2" = MuVariable $ name (currentDuty !! 1)
+    returnHtml $ hastacheFile defaultConfig "templates/index.html" (mkStrContext context)
+    
+    
 ---- Helper functions
 returnJson :: ToJSON a => IO a -> ActionM()
 returnJson ioV = do
-    val <- liftIO ioV
+    val <- liftAndCatchIO ioV
     json val
+
+returnHtml :: IO Text -> ActionM()
+returnHtml ioV = do
+    val <- liftAndCatchIO ioV
+    html val
