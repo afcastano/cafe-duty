8622d7b727f45e2099a978ac7c726727a28b0ade
afcastano <andresfelipecastano@gmail.com>
2017-01-20 01:02:44 +1100

    remove header x-frame-options

--- cafe-duty/src/Main.hs
+++ cafe-duty/src/Main.hs
@@ -1,10 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 import Web.Scotty (scotty, middleware)
+import Data.ByteString
 import Network.Wai.Middleware.Static (staticPolicy, noDots, addBase, (>->))
+import Network.Wai.Middleware.StripHeaders (stripHeader)
+import Network.Wai (Middleware, modifyResponse)
 
 import App.Api (webApi)
 
 main :: IO ()
 main = scotty 3000 $ do 
+  middleware $ removeHeader "X-FRAME-OPTIONS"
   middleware $ staticPolicy (noDots >-> addBase "static") -- set up the static folder
   webApi
+
+removeHeader :: ByteString -> Middleware
+removeHeader h = modifyResponse $ stripHeader h
